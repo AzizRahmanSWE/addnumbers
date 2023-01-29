abb1dcd0c102b64e61c2d13afb9df962b3b27dcc
Friies <bahram.parsa2019@gmail.com>
2023-01-28 21:07:00 -0500

    initial commit

---
+++ addNumbers.js
@@ -0,0 +1,15 @@
+
+// We are tasked to create a program that takes
+// in any number of command line arguments
+// which will be numbers, and adds them toghether
+// in and returns a sum of all the numbers.
+// node addNumbers 1 2 3 4 5
+// 15
+
+
+//how to take command line arguments.(terminal)
+
+//how to add multiple numbers together.
+
+//return/console.logged(totalNumber)
+console.log(process.argv)
