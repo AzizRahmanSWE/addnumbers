a9c393bbb7ab72461bfcbfb94fab72ed8db5a05a
Friies <bahram.parsa2019@gmail.com>
2023-01-28 21:20:15 -0500

    add numbers solution done

--- addNumbers.js
+++ addNumbers.js
@@ -12,4 +12,17 @@
 //how to add multiple numbers together.
 
 //return/console.logged(totalNumber)
-console.log(process.argv)
+console.log(process.argv) // argv after 2nd element is what I write to my command. compiling my file!
+
+//how do I go throught the list of numbers?(looping)
+//how to add numbers?
+
+let sum = 0
+for (let i = 2; i < process.argv.length; i++) {
+  console.log('i =', i);
+  console.log(process.argv[i]);
+  //gotta convert the string recieved from terminal to a number.
+  sum = sum + Number(process.argv[i]);
+}
+
+console.log("the value is ", sum);
