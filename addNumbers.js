6d3675771a05e8aa974685a29fbf1bb3eaa33beb
Friies <bahram.parsa2019@gmail.com>
2023-01-28 22:08:11 -0500

    make addNumbers into a function refactor

--- addNumbers.js
+++ addNumbers.js
@@ -3,28 +3,19 @@
 // in any number of command line arguments
 // which will be numbers, and adds them toghether
 // in and returns a sum of all the numbers.
+
 // node addNumbers 1 2 3 4 5
 // 15
 
 
-//how to take command line arguments.(terminal)
-
-//how to add multiple numbers together.
-
-//return/console.logged(totalNumber)
-// argv after 2nd element is what I write to my command. compiling my file!
-
-//how do I go throught the list of numbers?(looping)
-//how to add numbers?
-
-let sum = 0;
 const array = process.argv.splice(2);
 
-
-for (let i = 0; i < array.length; i++) { ///TODO: find a cleaner way to i = 2;
-  //gotta convert the string recieved from terminal to a number.
-  sum = sum + Number(array[i]);
+const addNumbers = function(banana) {
+  let sum = 0;
+  for (let i = 0; i < banana.length; i++) { 
+    sum = sum + Number(banana[i]);
+  }
+  console.log("the value is ", sum);
 }
-//Make a function to keep everthing clean.
 
-console.log("the value is ", sum);
+addNumbers(array);
