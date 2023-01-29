d55525a60ae209e64082a6e687873f957d97ecdf
Friies <bahram.parsa2019@gmail.com>
2023-01-28 23:39:07 -0500

    updates

--- addNumbers.js
+++ addNumbers.js
@@ -6,15 +6,24 @@
 
 // node addNumbers 1 2 3 4 5
 // 15
+//argv after 2nd element is what i write to my command
+//compiling my files
 
 
 const array = process.argv.splice(2);
 
 const addNumbers = function(banana) {
   let sum = 0;
-  for (let i = 0; i < banana.length; i++) { 
-    sum = sum + Number(banana[i]);
+  //for (let i = 0; i <array.length; i++)
+  //for (let i in banana) loops through the INDEX of array
+  //[44, 55, 33, 22, 11] i = 0, 1, 2, 3, 4, 5
+  // 0   1   2   3   4
+
+  for (let val of banana) { 
+    //gotta convert the string from the terminal to a number.
+    sum += Number(val);
   }
+
   console.log("the value is ", sum);
 }
 
